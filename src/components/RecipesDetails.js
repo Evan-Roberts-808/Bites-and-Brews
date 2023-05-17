bc8a049f161b29791243e8b684ab327bd2f6961c
Matthew Phan <mttphan261@gmail.com>
2023-05-17 15:07:44 -0500

    add search and filter

--- src/components/RecipesDetails.js
+++ src/components/RecipesDetails.js
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import Container from "react-bootstrap/Container";
 
 function RecipesDetails() {
@@ -25,20 +25,20 @@ function RecipesDetails() {
 
   //get <li> items for ingredients
   const mappedIngredients = ingredients.map((el) => {
-    return <li>{el}</li>
-  })
+    return <li>{el}</li>;
+  });
 
   //get <li> items for ingredients
   const mappedInstructions = instructions.map((el) => {
-    return <li>{el}</li>
-  })
+    return <li>{el}</li>;
+  });
 
   const handleLikeClick = () => {
     // Make a PATCH request to update the like count on the server
     fetch(`http://localhost:3001/recipes/${id}`, {
-      method: 'PATCH',
+      method: "PATCH",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         likes: recipeDetails.likes + 1, // Increment like count by 1
@@ -49,24 +49,30 @@ function RecipesDetails() {
         setLikeCount(data.likes); // Update like count in state with the updated value from the server
       })
       .catch((error) => {
-        console.error('Error:', error);
+        console.error("Error:", error);
       });
   };
 
   return (
     <Container>
       <img src={recipeDetails.image} alt={recipeDetails.name} />
-      <h2>{recipeDetails.name}</h2> <FontAwesomeIcon icon={faThumbsUp} onClick={handleLikeClick} style={{"--fa-primary-color": "#ff3b3f", "--fa-secondary-color": "#c52b30",}}/><p>{likeCount}</p>
+      <h2>{recipeDetails.name}</h2>{" "}
+      <FontAwesomeIcon
+        icon={faThumbsUp}
+        onClick={handleLikeClick}
+        style={{
+          "--fa-primary-color": "#ff3b3f",
+          "--fa-secondary-color": "#c52b30",
+        }}
+      />
+      <p>{likeCount}</p>
       <h4>{recipeDetails.description}</h4>
-
       <p>Prep Time: {recipeDetails.preptime}</p>
       <p>Cook Time: {recipeDetails.cooktime}</p>
       <p>Additional Time: {recipeDetails.waittime}</p>
       <p>Total Time: {recipeDetails.totaltime}</p>
-
       <h2>Ingredients</h2>
       <ul>{mappedIngredients}</ul>
-
       <h2>Instructions</h2>
       <ul>{mappedInstructions}</ul>
     </Container>
--- src/components/RecipesPage.js
+++ src/components/RecipesPage.js
@@ -1,23 +1,97 @@
-import React, {useState, useEffect} from 'react'
-import Cards from './Cards'
+import React, { useState, useEffect } from "react";
+import Cards from "./Cards";
 import Container from "react-bootstrap/Container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 function RecipesPage() {
-  const [allRecipes, setAllRecipes] = useState([])
+  const [allRecipes, setAllRecipes] = useState([]);
+  const [form, setForm] = useState("");
+  const [search, setSearch] = useState("");
+
+  const [cuisineFilter, setCuisineFilter] = useState("");
+
+  //offCanvas states
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const toggleShow = () => setShow((s) => !s);
 
   useEffect(() => {
-    fetch('http://localhost:3001/recipes')
-    .then(response => response.json())
-    .then(data => setAllRecipes(data))
-  }, [])
+    fetch("http://localhost:3001/recipes")
+      .then((response) => response.json())
+      .then((data) => setAllRecipes(data));
+  }, []);
 
-  console.log(allRecipes)
+  function handleChange(e) {
+    setForm(e.target.value);
+    console.log(form);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    handleSearch(form);
+  }
+
+  function handleSearch(newSearch) {
+    setSearch(newSearch);
+  }
+
+  function handleCuisineFilter(e) {
+    setCuisineFilter(e.target.value);
+  }
+
+  console.log(allRecipes);
+
+  const searchedRecipes = [...allRecipes].filter((el) => {
+    const searchMatch = el.name.toLowerCase().includes(search.toLowerCase());
+    const filterMatch = cuisineFilter === el.cuisine || cuisineFilter === "";
+    return searchMatch && filterMatch;
+  });
 
   return (
     <Container>
-      <Cards data={allRecipes}/>
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Filter</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <select name="cuisine" id="cuisine" onChange={handleCuisineFilter}>
+            <option value="">Select by cuisine</option>
+            <option value="japanese">Japanese</option>
+            <option value="chinese">Chinese</option>
+            <option value="korean">Korean</option>
+            <option value="thai">Thai</option>
+            <option value="italian">Italian</option>
+            <option value="greek">Greek</option>
+            <option value="mediterranean">Mediterranean</option>
+            <option value="mexican">Mexican</option>
+            <option value="spanish">Spanish</option>
+            <option value="french">French</option>
+            <option value="caribbean">Caribbean</option>
+            <option value="american">American</option>
+          </select>
+        </Offcanvas.Body>
+      </Offcanvas>
+      <div className="row">
+        <h2>Recipes</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={(e) => handleChange(e)}
+          />
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            style={{ color: "#FF3B3F" }}
+            type="submit"
+          />
+        </form>
+        <button onClick={toggleShow}>Filter</button>
+      </div>
+      <Cards data={searchedRecipes} />
     </Container>
-  )
+  );
 }
 
-export default RecipesPage
+export default RecipesPage;
